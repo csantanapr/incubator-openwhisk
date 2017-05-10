1671511c3b51e45b91972149b67e49b76373e33b
Carlos Santana <csantana@us.ibm.com>
2017-05-10 00:10:30 -0400

    capture couchdb after deployment

--- tools/jenkins/apache/dockerhub.groovy
+++ tools/jenkins/apache/dockerhub.groovy
@@ -27,52 +27,68 @@ node("ubuntu&&xenial") {
         ])
   }
 
-  stage("CouchDB Snapshot") {
-    withEnv(['PATH+LOCAL_JENKINS=/home/jenkins/.local/bin']) {
+  stage("OpenWhisk") {
+    def JAVA_JDK_8=tool name: 'JDK 1.8 (latest)', type: 'hudson.model.JDK'
+
+    withEnv(["Path+JDK=$JAVA_JDK_8/bin","JAVA_HOME=$JAVA_JDK_8",'PATH+LOCAL_JENKINS=/home/jenkins/.local/bin']) {
+      withCredentials([usernamePassword(credentialsId: 'csantanapr_dockerhub', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USER')]) {
+          sh 'docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}'
+      }
       sh "python --version"
       sh "ansible --version"
       sh "ansible-playbook --version"
-      dir('ansible') {
-        def ANSIBLE_CMD = "ansible-playbook -i environments/local"
-        sh "$ANSIBLE_CMD setup.yml"
-        sh "$ANSIBLE_CMD couchdb.yml"
-        sh "$ANSIBLE_CMD initdb.yml"
-        sh "$ANSIBLE_CMD wipe.yml"
-        sh "docker commit couchdb openwhisk/couchdb-snapshot"
-        sh "docker images | grep openwhisk"
-        sh "$ANSIBLE_CMD couchdb.yml -e mode=clean"
-      }
-    }
-  }
+      
+      def dockerhub_prefix = "csantanapr"
 
-  stage("Deploy DockerHub") {
-    def JAVA_JDK_8=tool name: 'JDK 1.8 (latest)', type: 'hudson.model.JDK'
-    withEnv(["Path+JDK=$JAVA_JDK_8/bin","JAVA_HOME=$JAVA_JDK_8"]) {
-      withCredentials([usernamePassword(credentialsId: 'openwhisk_dockerhub', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USER')]) {
-          sh 'docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}'
-      }
-      def PUSH_CMD = "./gradlew distDocker -PdockerRegistry=docker.io -PdockerImagePrefix=openwhisk -x tests:dat:blackbox:badproxy:distDocker -x tests:dat:blackbox:badaction:distDocker -x sdk:docker:distDocker -x tools:cli:distDocker -x tools:cli:distDocker"
+
+
+      def PUSH_CMD = "./gradlew distDocker -PdockerRegistry=docker.io -PdockerImagePrefix=${dockerhub_prefix} -x tests:dat:blackbox:badproxy:distDocker -x tests:dat:blackbox:badaction:distDocker -x sdk:docker:distDocker -x tools:cli:distDocker"
       def gitCommit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
       def shortCommit = gitCommit.take(7)
       sh "${PUSH_CMD} -PdockerImageTag=latest"
       sh "${PUSH_CMD} -PdockerImageTag=${shortCommit}"
-      sh "docker tag openwhisk/couchdb-snapshot openwhisk/couchdb-snapshot:latest"
-      sh "docker tag openwhisk/couchdb-snapshot openwhisk/couchdb-snapshot:${shortCommit}"
-      sh "docker push openwhisk/couchdb-snapshot"
+
+      dir('ansible') {
+        def ANSIBLE_CMD = "ansible-playbook -i environments/local"
+        sh "$ANSIBLE_CMD setup.yml"
+        sh "$ANSIBLE_CMD couchdb.yml -e mode=clean"
+        sh "$ANSIBLE_CMD couchdb.yml"
+        sh "$ANSIBLE_CMD initdb.yml"
+        sh "$ANSIBLE_CMD wipe.yml"
+        sh "$ANSIBLE_CMD apigateway.yml -e mode=clean"
+        sh "$ANSIBLE_CMD apigateway.yml"
+        sh "$ANSIBLE_CMD openwhisk.yml -e mode=clean"
+        sh "$ANSIBLE_CMD openwhisk.yml"
+        sh "$ANSIBLE_CMD postdeploy.yml"
+      }
+      cat whisk.properties
+      sh "docker images | grep openwhisk"
+      sh "docker ps"
+
+
+      sh "docker commit couchdb ${dockerhub_prefix}/couchdb-snapshot"
+      sh "docker tag ${dockerhub_prefix}/couchdb-snapshot ${dockerhub_prefix}/couchdb-snapshot:latest"
+      sh "docker tag ${dockerhub_prefix}/couchdb-snapshot ${dockerhub_prefix}/couchdb-snapshot:${shortCommit}"
+      sh "docker push ${dockerhub_prefix}/couchdb-snapshot"
 
     }
   }
 
   stage("Clean") {
+    sh "docker ps"
+    withEnv(['PATH+LOCAL_JENKINS=/home/jenkins/.local/bin']) {
+      dir('ansible') {
+        def ANSIBLE_CMD = "ansible-playbook -i environments/local"
+        sh "$ANSIBLE_CMD couchdb.yml -e mode=clean"
+        sh "$ANSIBLE_CMD apigateway.yml -e mode=clean"
+        sh "$ANSIBLE_CMD openwhisk.yml -e mode=clean"
+      }
+    }
+    sh "docker ps"
+
     sh "docker images"
     sh 'docker rmi -f $(docker images | grep openwhisk | awk \'{print $3}\') || true'
     sh "docker images"
   }
 
-  stage("Notify") {
-    withCredentials([string(credentialsId: 'openwhisk_slack_token', variable: 'OPENWHISK_SLACK_TOKEN')]) {
-      sh "curl -X POST --data-urlencode 'payload={\"channel\": \"#dev\", \"username\": \"whiskbot\", \"text\": \"OpenWhisk Docker Images build and posted to https://hub.docker.com/u/openwhisk by Jenkins job ${BUILD_URL}\", \"icon_emoji\": \":openwhisk:\"}' https://hooks.slack.com/services/${OPENWHISK_SLACK_TOKEN}"
-    }
-    
-  }
 }
