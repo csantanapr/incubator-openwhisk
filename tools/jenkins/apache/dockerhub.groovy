b0e7238a5d0b7de635f9fb28aae8f6ed3eaf396d
Carlos Santana <csantana@us.ibm.com>
2017-05-10 00:54:31 -0400

    become ansible

--- tools/jenkins/apache/dockerhub.groovy
+++ tools/jenkins/apache/dockerhub.groovy
@@ -27,27 +27,30 @@ node("ubuntu&&xenial") {
         ])
   }
 
-  stage("OpenWhisk") {
+  stage("Build OpenWhisk") {
     def JAVA_JDK_8=tool name: 'JDK 1.8 (latest)', type: 'hudson.model.JDK'
 
-    withEnv(["Path+JDK=$JAVA_JDK_8/bin","JAVA_HOME=$JAVA_JDK_8",'PATH+LOCAL_JENKINS=/home/jenkins/.local/bin']) {
+    withEnv(["Path+JDK=$JAVA_JDK_8/bin","JAVA_HOME=$JAVA_JDK_8"]) {
       withCredentials([usernamePassword(credentialsId: 'csantanapr_dockerhub', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USER')]) {
           sh 'docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}'
       }
-      sh "python --version"
-      sh "ansible --version"
-      sh "ansible-playbook --version"
       
       def dockerhub_prefix = "csantanapr"
-
-
-
+      /*
       def PUSH_CMD = "./gradlew distDocker -PdockerRegistry=docker.io -PdockerImagePrefix=${dockerhub_prefix} -x tests:dat:blackbox:badproxy:distDocker -x tests:dat:blackbox:badaction:distDocker -x sdk:docker:distDocker -x tools:cli:distDocker"
       def gitCommit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
       def shortCommit = gitCommit.take(7)
       sh "${PUSH_CMD} -PdockerImageTag=latest"
       sh "${PUSH_CMD} -PdockerImageTag=${shortCommit}"
+      */
 
+    }
+  }
+  stage("Deploy OpenWhisk") {
+    withEnv(['PATH+LOCAL_JENKINS=/home/jenkins/.local/bin']) {
+      sh "python --version"
+      sh "ansible --version"
+      sh "ansible-playbook --version"
       dir('ansible') {
         def ANSIBLE_CMD = "ansible-playbook -i environments/local"
         sh "$ANSIBLE_CMD setup.yml"
@@ -55,7 +58,7 @@ node("ubuntu&&xenial") {
         sh "$ANSIBLE_CMD couchdb.yml"
         sh "$ANSIBLE_CMD initdb.yml"
         sh "$ANSIBLE_CMD wipe.yml"
-        sh "$ANSIBLE_CMD apigateway.yml -e mode=clean"
+        sh "$ANSIBLE_CMD apigateway.yml -e mode=clean --become-user=$USER"
         sh "$ANSIBLE_CMD apigateway.yml"
         sh "$ANSIBLE_CMD openwhisk.yml -e mode=clean"
         sh "$ANSIBLE_CMD openwhisk.yml"
@@ -65,16 +68,22 @@ node("ubuntu&&xenial") {
       sh "docker images | grep openwhisk"
       sh "docker ps"
 
-
-      sh "docker commit couchdb ${dockerhub_prefix}/couchdb-snapshot"
-      sh "docker tag ${dockerhub_prefix}/couchdb-snapshot ${dockerhub_prefix}/couchdb-snapshot:latest"
-      sh "docker tag ${dockerhub_prefix}/couchdb-snapshot ${dockerhub_prefix}/couchdb-snapshot:${shortCommit}"
-      sh "docker push ${dockerhub_prefix}/couchdb-snapshot"
-
     }
   }
 
-  stage("Clean") {
+  stage("Snapshot OpenWhisk CouchDB") {
+    def dockerhub_prefix = "csantanapr"
+    sh "docker images | grep openwhisk"
+    sh "docker ps"
+    sh "docker commit couchdb ${dockerhub_prefix}/couchdb-snapshot"
+    sh "docker tag ${dockerhub_prefix}/couchdb-snapshot ${dockerhub_prefix}/couchdb-snapshot:latest"
+    sh "docker tag ${dockerhub_prefix}/couchdb-snapshot ${dockerhub_prefix}/couchdb-snapshot:${shortCommit}"
+    sh "docker push ${dockerhub_prefix}/couchdb-snapshot"
+  }
+
+
+
+  stage("Clean OpenWhisk") {
     sh "docker ps"
     withEnv(['PATH+LOCAL_JENKINS=/home/jenkins/.local/bin']) {
       dir('ansible') {
@@ -89,6 +98,7 @@ node("ubuntu&&xenial") {
     sh "docker images"
     sh 'docker rmi -f $(docker images | grep openwhisk | awk \'{print $3}\') || true'
     sh "docker images"
+    sh "docker logout"
   }
 
 }
